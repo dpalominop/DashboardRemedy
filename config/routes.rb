ff776ae380da27abfd3498791ef20cd7a5ce99f5
Daniel Palomino <dapalominop@gmail.com>
2017-06-20 16:15:31 -0500

    Configurado AcriveAdmin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, 
-        path: 'auth', 
-        controllers: {
-          sessions: 'users/sessions'
-        },
-        path_names: { 
-          sign_in: 'login', 
-          sign_out: 'logout', 
-          password: 'secret', 
-          confirmation: 'verification', 
-          unlock: 'unblock', 
-          registration: 'register', 
-          sign_up: 'cmon_let_me_in' }
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
