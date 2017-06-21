08751a75c957df2fe2e7ee9250e7b8708bdf1929
Daniel Palomino <dapalominop@gmail.com>
2017-06-21 13:28:41 -0500

    Cambio de ruta raiz en apliación.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :employees
   resources :platforms
   devise_for :users, ActiveAdmin::Devise.config
+  root to: "admin/dashboard#index"
   ActiveAdmin.routes(self)
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
